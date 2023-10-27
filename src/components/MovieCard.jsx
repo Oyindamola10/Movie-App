5b6c6142b4f9e95b653c2f860f78a646d801171f
Oyindamola10 <samiatadegbenro@gmail.com>
2023-10-27 14:49:45 +0100

    used react router

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
+
+import { Link } from "react-router-dom"
 import "./MovieCard.css"
 
-const MovieCard = ({ title, description, posterURL, rating }) => {
+const MovieCard = ({ title, description, rating,trailerURL, id}) => {
   return (
-    <div className="container">
-      <img src={posterURL} alt= "" />
+    
+      <Link to={`/movie/${id}`}>
+      <div className="container">
+      
+      <iframe src={trailerURL} allowFullScreen/>
       <h2>{title}</h2>
       <p>{description}</p>
       <p>Rating:{rating}</p>
+      
     </div>
+    </Link>
+    
   )
 
   
